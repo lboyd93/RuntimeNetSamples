2369022047aa4dc563c3bb5a1f33971e2e3db04b
lboyd93 <lboyd@esri.com>
2019-12-31 15:30:00 -0800

    Added Xamarin.iOS sample to add feature or map service to map.

---
+++ iOS/AddLayer/AddLayer/ViewController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using Esri.ArcGISRuntime.Data;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Location;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Security;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.Tasks;
+using Esri.ArcGISRuntime.UI;
+using Esri.ArcGISRuntime.UI.Controls;
+using UIKit;
+
+namespace AddLayer
+{
+    public partial class ViewController : UIViewController
+    {
+        MapViewModel _mapViewModel = new MapViewModel();
+        MapView _mapView;
+
+        string _serviceURL = "";
+        Envelope _extent;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Create a new map view, set its map, and provide the coordinates for laying it out
+            _mapView = new MapView()
+            {
+                //Map = _mapViewModel.Map // Use the map from the view-model
+                Map = new Map(Basemap.CreateTopographic())
+            };
+
+            // Add the MapView to the Subview
+            View.AddSubview(_mapView);
+
+            View.AddSubview(InputServiceURL);
+            View.AddSubview(AddLayer);
+            View.AddSubview(ClearButton);
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            // Fill the screen with the map
+            _mapView.Frame = new CoreGraphics.CGRect(0, 0, View.Bounds.Width, View.Bounds.Height);
+            base.ViewDidLayoutSubviews();
+        }
+
+        private void MapViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // Update the map view with the view model's new map
+            if (e.PropertyName == "Map" && _mapView != null)
+                _mapView.Map = _mapViewModel.Map;
+        }
+
+        partial void AddLayer_TouchUpInside(UIButton sender)
+        {
+            _serviceURL = InputServiceURL.Text;
+            if (_serviceURL.Length <= 4)
+            {
+                //Create Alert
+                var okAlertController = UIAlertController.Create("No URL Input", "Please type in a URL in the text box.", UIAlertControllerStyle.Alert);
+                //Add Action
+                okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+                // Present Alert
+                PresentViewController(okAlertController, true, null);
+            }
+            else
+            {
+                
+                if (_serviceURL.Substring(0, 4) != "http")
+                {
+                    //Create Alert
+                    var okAlertController = UIAlertController.Create("Invalid URL", "Please input a valid URL", UIAlertControllerStyle.Alert);
+                    //Add Action
+                    okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+                    // Present Alert
+                    PresentViewController(okAlertController, true, null);
+                }
+                else
+                {
+                    Uri serviceURI = new Uri(_serviceURL);
+
+                    if (_serviceURL.Contains("MapServer"))
+                    {
+                        ArcGISMapImageLayer mapImageLayer = new ArcGISMapImageLayer(serviceURI);
+                        mapImageLayer.LoadAsync();
+                        //_extent = mapImageLayer.FullExtent;
+                        _mapView.Map.OperationalLayers.Add(mapImageLayer);
+                    }
+                    else if (_serviceURL.Contains("FeatureServer"))
+                    {
+
+                        FeatureLayer featurelayer = new FeatureLayer(serviceURI);
+                        featurelayer.LoadAsync();
+                        //_extent = featurelayer.FullExtent;
+                        _mapView.Map.OperationalLayers.Add(featurelayer);
+                        
+                    }
+                    else
+                    {
+                        //Create Alert
+                        var okAlertController = UIAlertController.Create("Invalid Service Type", "Please input a valid map service or feature service.", UIAlertControllerStyle.Alert);
+                        //Add Action
+                        okAlertController.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+                        // Present Alert
+                        PresentViewController(okAlertController, true, null);
+                    }
+
+                    //_mapView.SetViewpointGeometryAsync(_extent);
+
+                }
+            }
+
+
+        }
+
+        partial void ClearButton_TouchUpInside(UIButton sender)
+        {
+            _mapView.Map.OperationalLayers.Clear();
+        }
+    }
+}
---
+++ iOS/AddLayer/AddLayer/ViewController.designer.cs
@@ -0,0 +1,54 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace AddLayer
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton AddLayer { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ClearButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField InputServiceURL { get; set; }
+
+        [Action ("AddLayer_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void AddLayer_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("ClearButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void ClearButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (AddLayer != null) {
+                AddLayer.Dispose ();
+                AddLayer = null;
+            }
+
+            if (ClearButton != null) {
+                ClearButton.Dispose ();
+                ClearButton = null;
+            }
+
+            if (InputServiceURL != null) {
+                InputServiceURL.Dispose ();
+                InputServiceURL = null;
+            }
+        }
+    }
+}
